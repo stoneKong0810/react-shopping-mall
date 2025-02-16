3907acef0353d4a73f902da91e901a8b1c6e3afd
stoneKong0810 <stonekong2115@gmail.com>
2025-02-16 21:52:13 +0900

    refactor : id 별로 다른 화면 출력

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,42 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState } from 'react';
 import shoesData from './data/data.js';
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Detail from './pages/Detail.jsx';
 import Main from './pages/Main.jsx';
 
 function App() {
 	const [shoes] = useState(shoesData);
+	const navigate = useNavigate();
 
 	return (
 		<>
 			<Navbar bg="light" data-bs-theme="light">
 				<Container>
-					<Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
+					<Navbar.Brand>Shoe Shop</Navbar.Brand>
 					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#cart">Cart</Nav.Link>
+						<Nav.Link
+							onClick={() => {
+								navigate('/');
+							}}
+						>
+							Home
+						</Nav.Link>
+						<Nav.Link
+							onClick={() => {
+								navigate('/detail');
+							}}
+						>
+							Detail
+						</Nav.Link>
 					</Nav>
 				</Container>
 			</Navbar>
 
 			<Routes>
+				<Route path="*" />
 				<Route path="/" element={<Main shoes={shoes} />} />
-				<Route path="/detail" element={<Detail />} />
+				<Route path="/detail/:id" element={<Detail shoes={shoes} />} />
 			</Routes>
 		</>
 	);
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,18 +1,20 @@
-function Detail() {
+import { useParams } from 'react-router-dom';
+
+function Detail({ shoes }) {
+	const { id } = useParams();
+	const shoeId = parseInt(id, 10);
+	const shoe = shoes.find((shoe) => shoe.id === shoeId);
+
 	return (
 		<div className="container">
 			<div className="row">
 				<div className="col-md-6">
-					<img
-						src="https://codingapple1.github.io/shop/shoes1.jpg"
-						alt="상품"
-						width="100%"
-					/>
+					<img src={`${shoe.image}`} alt={shoe.title} width="100%" />
 				</div>
 				<div className="col-md-6">
-					<h4 className="pt-5">상품명</h4>
-					<p>상품설명</p>
-					<p>120000원</p>
+					<h4 className="pt-5">{shoe.title}</h4>
+					<p>{shoe.content}</p>
+					<p>{shoe.price}원</p>
 					<button className="btn btn-danger">주문하기</button>
 				</div>
 			</div>
