8ab8fcb7f39f800cd0a12ec056d44669d84fdb1f
stoneKong0810 <stonekong2115@gmail.com>
2025-02-17 19:57:49 +0900

    add : useEffect 사용

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,26 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function Detail({ shoes }) {
 	const { id } = useParams();
 	const shoeId = parseInt(id, 10);
 	const shoe = shoes.find((shoe) => shoe.id === shoeId);
+	const [alert, setAlert] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setAlert(false);
+		}, 2000);
+		return () => {
+			clearTimeout(timer);
+		};
+	}, []);
 
 	return (
 		<div className="container">
+			{alert === true ? (
+				<div className="alert alert-info">2초 이내 구매시 할인!</div>
+			) : null}
 			<div className="row">
 				<div className="col-md-6">
 					<img src={`${shoe.image}`} alt={shoe.title} width="100%" />
