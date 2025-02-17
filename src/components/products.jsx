aefad8fe312b1c1bb6d9f917264db807355e2cda
stoneKong0810 <stonekong2115@gmail.com>
2025-02-17 18:54:30 +0900

    fix : 이미지 클릭시 해당 제품 디테일 페이지로 이동

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
 function Products({ shoes }) {
+	const navigate = useNavigate();
 	const renderShoe = (shoe, index) => (
 		<div className="col-md-4 text-center" key={index}>
-			<img src={shoe.image} alt={shoe.name} />
+			<img
+				src={shoe.image}
+				alt={shoe.name}
+				onClick={() => {
+					navigate(`/detail/${shoe.id}`);
+				}}
+			/>
 			<h4>{shoe.title}</h4>
 			<p>{shoe.content}</p>
 		</div>
